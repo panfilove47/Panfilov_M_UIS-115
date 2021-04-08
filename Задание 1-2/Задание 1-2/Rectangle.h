49a06c1b6d5a625558c439ad539cebd5c8b6af73
panfilove47 <mixailpanf@yandex.ru>
2021-04-08 23:04:51 +0300

    Переписан класс Rectangle

--- Задание 1-2/Задание 1-2/Rectangle.h
+++ Задание 1-2/Задание 1-2/Rectangle.h
@@ -1,63 +1,68 @@
 #pragma once
-#include <iostream>
-#include <sstream>
-#include <math.h>
+#include <iostream> 
 using namespace std;
 
-
-class Rectangle
-{
-public:
-	Rectangle(); //Объявление конструктора
-	~Rectangle(); //Объявление деструктора
-	void Emp_show(void);
-	void Pme_show(void);
-	int LeftAngleX;
-	int LeftAngleY;
-	int RightAngleX;
-	int RightAngleY;
-	int dlina;
-	int shirina;
-	int S;
+class Rectangle {
 private:
-};
-//Инициализация конструктора
-Rectangle::Rectangle()
-{
-}
-Rectangle::~Rectangle()
-{
-}
-void Rectangle::Emp_show(void)
-{
-	setlocale(0, "");
-	cout << "upper-left corner by x: " << LeftAngleX << endl;
-	cout << "upper-left corner by y: " << LeftAngleY << endl;
-	cout << "lower-right corner by x: " << RightAngleX << endl;
-	cout << "lower-right corner by y: " << RightAngleY << endl;
-	dlina = LeftAngleX - RightAngleX;
-	dlina = abs(dlina);
-	shirina = LeftAngleY - RightAngleY;
-	shirina = abs(shirina);
-	S = dlina * shirina;
-	cout << "Length : " << dlina << endl;
-	cout << "Width : " << shirina << endl;
-	cout << "Area: " << S << endl;
-}
-void Rectangle::Pme_show(void)
-{
-	dlina = LeftAngleX - RightAngleX;
-	dlina = abs(dlina);
-	shirina = LeftAngleY - RightAngleY;
-	shirina = abs(shirina);
-	S = dlina * shirina;
-	cout << "upper-left corner by x: " << LeftAngleX << endl;
-	cout << "upper-left corner by y: " << LeftAngleY << endl;
-	cout << "lower-right corner by x: " << RightAngleX << endl;
-	cout << "lower-right corner by y: " << RightAngleY << endl;
-	cout << "Length: " << dlina << endl;
-	cout << "Width: " << shirina << endl;
-	cout << "Area: " << S << endl;
-}
+    double x1;
+    double y1;
+    double x2;
+    double y2;
 
+public:
+    Rectangle()
+    {
+        x1 = 0.0;
+        y1 = 0.0;
+        x2 = 0.0;
+        y2 = 0.0;
+    }
+    Rectangle(double a_x1, double a_y1, double a_x2, double a_y2)
+    {
+        x1 = a_x1;
+        y1 = a_y1;
+        x2 = a_x2;
+        y2 = a_y2;
+    }
+    double getcoordinate_displayX1()
+    {
+        return x1;
+    }
+    double getcoordinate_displayX2()
+    {
+        return x2;
+    }
+    double getcoordinate_displayY1()
+    {
+        return y1;
+    }
+    double getcoordinate_displayY2()
+    {
+        return y2;
+    }
+
+    double getHeight() const
+    {
+        return abs(y1 - y2);
+    }
+
+    double getWidth() const
+    {
+        return abs(x1 - x2);
+    }
+
+    double getArea() const
+    {
+        return (abs(x1 - x2)) * (abs(y1 - y2));
+    }
+    double getMoving(double moveX, double moveY)
+    {
+        x1 = x1 + moveX;
+        x2 = x2 + moveX;
+        y1 = y1 + moveY;
+        y2 = y2 + moveY;
+        return x1, x2, y1, y2;
+    }
+    ~Rectangle() {};
+};
 
--- Задание 1-2/Задание 1-2/main.cpp
+++ Задание 1-2/Задание 1-2/main.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
-#include <sstream>
-#include <math.h>
-using namespace std;
 #include "Rectangle.h"
+using namespace std;
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	int x1, y1, x2, y2, z;
-	Rectangle EnteringCoordinates;
-	cout << "Enter x1" << endl;
-	cin >> x1;
-	cout << "Enter y1" << endl;
-	cin >> y1;
-	cout << "Enter x2" << endl;
-	cin >> x2;
-	cout << "Enter y2" << endl;
-	cin >> y2;
-	EnteringCoordinates.LeftAngleX = x1;
-	EnteringCoordinates.LeftAngleY = y1;
-	EnteringCoordinates.RightAngleX = x2;
-	EnteringCoordinates.RightAngleY = y2;
-	EnteringCoordinates.Emp_show();
-
-	Rectangle Move;
-	cout << "Enter the move: " << endl;
-	cin >> z;
-	Move.LeftAngleX = x1 + z;
-	Move.LeftAngleY = y1 + z;
-	Move.RightAngleX = x2 + z;
-	Move.RightAngleY = y2 + z;
-	Move.Pme_show();
-	system("Pause");
-	return 0;
+	double x1, x2, y1, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	Rectangle rect(x1, y1, x2, y2);
+	cout << "Top left corner: " << "(" << rect.getcoordinate_displayX1() << ";" << rect.getcoordinate_displayY1() << ")" << endl;
+	cout << "Bottom right corner: " << "(" << rect.getcoordinate_displayX2() << ";" << rect.getcoordinate_displayY2() << ")" << endl;
+	cout << rect.getHeight() << endl;
 }
